2c894dea1636ae696a2bd5175ddc1cf2c070f45a
Ilia <i.ilmer@yahoo.com>
2025-04-27 20:37:04 -0500

    run individual token count, pair count

--- c/main.c
+++ c/main.c
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 // TODO: Tokenization:
-// • Implement pair counting clearly (pair → frequency).
-// • Merge the most frequent pair iteratively.
+// * Implement pair counting clearly (pair → frequency).
+// * Merge the most frequent pair iteratively.
 void print_hex(const char *s);
 int main(void) {
-  // • ✅ Implement vocabulary hash table clearly (token_sequence → frequency).
-  char text[] = "low lower lowest";
+  // * ✅ Implement vocabulary hash table clearly (token_sequence → frequency).
+  const char *readonly_text = "low lower lowest";
+  char *text = strdup(readonly_text);
   ht *vocab = ht_create();
   bool insertion;
   char *split_string = strtok(text, " ");
@@ -25,6 +26,7 @@ int main(void) {
     split_string = strtok(NULL, " ");
     ht_item item;
     item_init(&item, tokens, sizeof(token) * n, 1);
+    item.item_type = KEY_TYPE_TOKEN;
     insertion = ht_insert_item(vocab, item);
     if (!insertion) {
       printf("Error inserting: %s\n", split_string);
@@ -32,26 +34,39 @@ int main(void) {
     }
   }
   ht_display(vocab);
+  ht *pair_counter = ht_create();
+  pair_counter->hash_function = fnv1_hash_pair;
+  size_t token_size = 1;
+  text = strdup(readonly_text);
+
+  // * pair counting
+  for (size_t i = 0; i < strlen(text) - 1; i++) {
+    token l, r;
+    pair p;
+    l.data = malloc(sizeof(char) * token_size);
+    l.data[0] = text[i];
+    l.len = token_size;
+    r.data = malloc(sizeof(char) * token_size);
+    r.data[0] = text[i + 1];
+    r.len = token_size;
+    p.l = l;
+    p.r = r;
+    ht_item *found = ht_get_item_pair(pair_counter, &p, sizeof(pair));
+    if (found == NULL) {
+      ht_item token_pair;
+      item_init(&token_pair, &p, sizeof(pair), 1); // should copy not just the pointer addresses, but full l.data and r.data too ideally for full safety.
+      token_pair.item_type = KEY_TYPE_PAIR;
+      if (!ht_insert_item_pair(pair_counter, token_pair)) {
+        printf("Error inserting pair\n");
+        exit(1);
+      }
+    } else {
+      found->value += 1;
+    }
+  }
+  ht_display(pair_counter);
+  ht_destroy(pair_counter);
+  ht_destroy(vocab);
+
   return 0;
-  // create a table that counts pairs of characters in a string
-  // for (int i = 0; i < (int)strlen(text) - 1; i++) {
-  //   p.l = text[i];
-  //   p.r = text[i + 1];
-  //   ht_item item;
-  //   item_init(&item, &p, sizeof(p), 0);
-  //   insertion = ht_insert_item(table, item);
-  //   if ((!insertion) || (table->load_factor > 0.75)) {
-  //     // printf("LOAD FACTOR %f, resizing\n", table->load_factor);
-  //     ht_resize(table);
-  //     // printf("LOAD FACTOR AFTER RESIZE: %f\n", table->load_factor);
-  //   }
-  // }
-  // ht_display(table);
-  // printf("\n\n\n");
-  // p.l = 'n';
-  // p.r = 'y';
-  // ht_item get_item = *(ht_get_item(table, &p, sizeof(p)));
-  // item_display(get_item);
-  // printf("\n\n\n");
-  // return 0;
 }
