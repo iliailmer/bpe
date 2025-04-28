36bb9c7a3d3231e3a664f9926a0795ce823e24f2
Ilia <i.ilmer@yahoo.com>
2025-04-27 20:36:20 -0500

    reflect necessary changes

--- c/hasht.h
+++ c/hasht.h
@@ -23,11 +23,18 @@ typedef struct {
   token r;
 } pair;
 
+typedef enum {
+  KEY_TYPE_PAIR,
+  KEY_TYPE_TOKEN,
+  KEY_TYPE_STRING
+} key_type_t;
+
 typedef struct {
   void *key;
   size_t key_len;
   int value;
   bool occupied;
+  key_type_t item_type;
 } ht_item;
 
 typedef struct {
@@ -40,19 +47,25 @@ typedef struct {
 // item routines
 void item_display(ht_item item);
 void item_init(ht_item *item, void *key, size_t key_len, uint64_t value);
+void item_destroy(ht_item *item);
 
 // hashing routines
 uint64_t fnv1_hash(void *key, size_t _size);
 uint64_t fnv1a_hash(void *key, size_t _size);
 
+uint64_t fnv1_hash_pair(void *key, size_t _size);
+uint64_t fnv1a_hash_pair(void *key, size_t _size);
+
 // hash table routines
 void ht_display(ht *table);
 ht *ht_create(void);
+void ht_destroy(ht *table);
 bool ht_resize(ht *table);
 bool ht_init(ht **table); // trying out double pointer thing, will see if it works
 bool ht_insert_item(ht *table, ht_item item);
+bool ht_insert_item_pair(ht *table, ht_item item);
 ht_item *ht_get_item(ht *table, void *key, size_t _size);
+ht_item *ht_get_item_pair(ht *table, void *key, size_t _size);
 size_t ht_len(ht table);
 
 // bool ht_get_item(ht *table, void *key, size_t _size, ht_item *out);
-bool ht_destroy(ht *table);
